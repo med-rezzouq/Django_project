363e5be0e84585e7b7dc32b6f100f09a2b9c2496
rezzouq <mohammed.rezzouq@siway.ma>
2022-08-27 15:21:25 +0100

    ci(Generic list view): is replacing to create index function to show all model iotem : the generic item will define everything including template name and passing object

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index,name='book.all'),
+    path('', views.BookListView.as_view(),name='book.all'),
     path('<int:id>', views.show,name='book.show'),
     path('<int:id>/review', views.review,name='book.review'),
 ]
